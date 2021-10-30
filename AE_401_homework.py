a4b5bb47e05111722a169712b89368d286ce4075
aso239 <93032355+aso239@users.noreply.github.com>
2021-10-30 17:59:28 +0800

    Add files via upload

---
+++ AE_401_homework.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+m=input()
+e=input()
+m=int(m)
+e=int(e)
+if m>90 and e>90:
+    print("有獎品")
+elif m<60 and e<60:
+    print("要處罰")
+elif m<60 or e<60:
+    print("再加油")
+else:
+    print("多多複習")
+    
+
+    
+
+
+
