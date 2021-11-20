da15949e0730a6803bcb0000bc9a234fe765d991
aso239 <93032355+aso239@users.noreply.github.com>
2021-11-20 17:46:25 +0800

    Add files via upload

---
+++ AE_401_homework.py_6.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 20 16:08:28 2021
+
+@author: USER
+"""
+score=[]
+for i in range(1,6,1):
+    x=input()
+    x=int(x)
+    score.append(x)
+print(*score)
+sum=0
+for i in range(0,5,1):
+    sum=sum+score[i]
+print(sum/5)
+a=[1,2,3,4,5]
+m=0
+for i in range(0,5,1,):
+    if(m<a[i]):
+        m=a[i]
+print(m)
+a=[1,2,3,4,5]
+m=0
+for i in range(0,5,1,):
+    if(m>a[i]):
+        m=a[i]
+print(m)
+
+
+
