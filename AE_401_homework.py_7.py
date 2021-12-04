1b40829e6f8cb5bebdcb1c9e23566c4bf2734749
aso239 <93032355+aso239@users.noreply.github.com>
2021-12-04 16:32:26 +0800

    Add files via upload

--- AE_401_homework.py_7.py
+++ AE_401_homework.py_7.py
@@ -4,26 +4,21 @@ Created on Sat Nov 20 16:08:28 2021
 
 @author: USER
 """
-name = [["a",100],["b",99],["c",88],["d",77],["e",66]]
-sum=0
-for i in range(1,10,2):
-    sum=sum+name[i]
-print(sum/5)
+score = [["a",100],["b",99],["c",88]]
+m=9999999
+m_index=0
+for i in range(0,3,1):
+    if(m>score[i][1]):
+        m_index=i
+        m=score[i][1]
+print(score[m_index][0],m)
 m=0
-max_k=0
-small_k=0
-for i in range(1,10,2):
-    if(m<name[i]):
-        m=name[i]
-        max_k=i
-print(name[max_k-1])
-print(m)
-for i in range(1,10,2):
-    if(m>name[i]):
-        m=name[i]
-        small_k=i
-print(name[small_k-1])
-print(m)
+m_index=0
+for i in range(0,3,1):
+    if(m<score[i][1]):
+        m_index=i
+        m=score[i][1]
+print(score[m_index][0],m)
 
 
 
