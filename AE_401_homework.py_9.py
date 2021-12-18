d2d4395ff38ba3de5d6ccc161361534a9409b942
aso239 <93032355+aso239@users.noreply.github.com>
2021-12-18 17:48:47 +0800

    Add files via upload

---
+++ AE_401_homework.py_9.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 18 16:36:46 2021
+
+@author: USER
+"""
+while True: 
+     f=int(input())
+     if n==1:
+        g=int(input())
+        k=int(input())
+        print(g,"個蘋果一個",k,"元")
+     elif n==2: 
+        n=int(input())
+        print("今天進了",n,"個蘋果")
+        print("總共有",g+n,"個蘋果")
+     elif n==3:   
+        s=int(input())
+        print("賣",s,"個蘋果","應收",s*k,"元")
+        print("總共有",g+n-s,"個蘋果")
+     elif n==5:
+          print("總共有",g+n-s,"個蘋果")
+     elif n==6:
+         break
+接下來就不會了😓
+         
+         
